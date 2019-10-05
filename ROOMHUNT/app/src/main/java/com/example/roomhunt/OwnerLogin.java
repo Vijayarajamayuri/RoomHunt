49c18403bf31096bc53262d2c32a89b9d3d182d2
mohammadfarheen <farheen.chinnu01@gmail.com>
2019-10-04 21:33:01 -0500

    farheen

--- ROOMHUNT/app/src/main/java/com/example/roomhunt/OwnerLogin.java
+++ ROOMHUNT/app/src/main/java/com/example/roomhunt/OwnerLogin.java
@@ -2,23 +2,22 @@ package com.example.roomhunt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class owner extends AppCompatActivity {
+public class OwnerLogin extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_owner);
-        Button ownerlogin = findViewById(R.id.button4);
+        setContentView(R.layout.activity_owner_login2);
+        Button ownerlogin = findViewById(R.id.button20);
         ownerlogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(owner.this, OwnerLogin.class);
+                Intent intent = new Intent(OwnerLogin.this, Blocks.class);
                 startActivity(intent);
             }
         });
