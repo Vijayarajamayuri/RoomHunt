8c32f9d4c6014466788719da8d06a93da6ce274f
pathuribhavani <bavani.pathuri@gmail.com>
2019-10-04 19:09:00 -0500

    Bhavani Pathuri

---
+++ ROOMHUNT/app/src/main/java/com/example/roomhunt/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.roomhunt;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        addListenerOnButton();
+        addListenerOnButton1();
+    }
+    private void addListenerOnButton() {
+        final Context context = this;
+        Button owner = findViewById(R.id.button3);
+        owner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, owner.class);
+                    startActivity(intent);
+            }
+        });
+    }
+    public void addListenerOnButton1(){
+        final Context context = this;
+        Button signup = findViewById(R.id.button);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, SignUp.class);
+                startActivity(intent);
+            }
+        });
+    }
+    }
