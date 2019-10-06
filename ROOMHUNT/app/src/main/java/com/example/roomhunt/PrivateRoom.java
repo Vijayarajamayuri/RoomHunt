52db192e29583e600b98f39d3e542ee7cec507d7
Vijayarajamayuri <avrmayuri74@gmail.com>
2019-10-06 16:22:39 -0500

    specifications Activity

--- ROOMHUNT/app/src/main/java/com/example/roomhunt/PrivateRoom.java
+++ ROOMHUNT/app/src/main/java/com/example/roomhunt/PrivateRoom.java
@@ -2,7 +2,11 @@ package com.example.roomhunt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PrivateRoom extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class PrivateRoom extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_private_room);
+        addListenerOnButton1();
+    }
+    private void addListenerOnButton1() {
+        final Context context = this;
+        Button other = findViewById(R.id.button16);
+        other.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, Specications.class);
+                startActivity(intent);
+            }
+        });
     }
 }
