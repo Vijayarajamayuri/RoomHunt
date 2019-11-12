bdc75bc19bbff8bf7669de2aaacd933bedeb1ab4
Vijayarajamayuri <avrmayuri74@gmail.com>
2019-11-11 20:31:34 -0600

    Added firebase to the Application

--- ROOMHUNT/app/src/main/java/com/example/roomhunt/MainActivity.java
+++ ROOMHUNT/app/src/main/java/com/example/roomhunt/MainActivity.java
@@ -1,14 +1,12 @@
 package com.example.roomhunt;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,58 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        addListenerOnButton();
-        addListenerOnButton1();
-        addListenerOnButton2();
-        addListenerOnButton3();
-
-
     }
 
-    private void addListenerOnButton() {
-        final Context context = this;
-        Button owner = findViewById(R.id.button3);
-        owner.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(context, owner.class);
-                startActivity(intent);
-            }
-        });
-    }
-
-    public void addListenerOnButton1() {
-        final Context context = this;
-        Button signup = findViewById(R.id.button);
-        signup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(context, SignUp.class);
-                startActivity(intent);
-            }
-        });
-    }
-
-    public void addListenerOnButton2() {
-        final Context context = this;
-        Button signup = findViewById(R.id.button2);
-        signup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(context, user_login.class);
-                startActivity(intent);
-            }
-        });
-    }
-    public void addListenerOnButton3() {
-        final Context context = this;
-        Button fp = findViewById(R.id.button6);
-        fp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(context, ForgotPassword.class);
-                startActivity(intent);
-            }
-        });
-    }
+   // public void logout(View view) {
+   //     FirebaseAuth.getInstance().signOut();//logout
+    //    startActivity(new Intent(getApplicationContext(),Login.class));
+    //    finish();
+   // }
 }
