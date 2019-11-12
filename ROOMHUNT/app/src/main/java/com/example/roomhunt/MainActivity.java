ecc33d6a41fad580274b6272e347400712aa5740
Vijayarajamayuri <avrmayuri74@gmail.com>
2019-11-11 20:33:08 -0600

    Created Login

--- ROOMHUNT/app/src/main/java/com/example/roomhunt/MainActivity.java
+++ ROOMHUNT/app/src/main/java/com/example/roomhunt/MainActivity.java
@@ -16,9 +16,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-   // public void logout(View view) {
-   //     FirebaseAuth.getInstance().signOut();//logout
-    //    startActivity(new Intent(getApplicationContext(),Login.class));
-    //    finish();
-   // }
+   public void logout(View view) {
+    FirebaseAuth.getInstance().signOut();//logout
+    startActivity(new Intent(getApplicationContext(),Login.class));
+    finish();
+    }
 }
