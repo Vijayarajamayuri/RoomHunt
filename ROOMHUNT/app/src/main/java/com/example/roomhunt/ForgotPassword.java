e7e406036a8ece2fd65a4b6ecc3d5abad5fe8154
pathuribhavani <bavani.pathuri@gmail.com>
2019-11-15 22:00:04 -0600

    Pathuri Bhavani

--- ROOMHUNT/app/src/main/java/com/example/roomhunt/ForgotPassword.java
+++ ROOMHUNT/app/src/main/java/com/example/roomhunt/ForgotPassword.java
@@ -29,8 +29,32 @@ public class ForgotPassword extends AppCompatActivity {
         btnResetPass = (Button) findViewById(R.id.btn_reset_password);
         buttonBack = (Button) findViewById(R.id.btn_back);
         mAuth = FirebaseAuth.getInstance();
+        btnResetPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email = editEmail.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplicationContext(), "Enter your email!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                mAuth.sendPasswordResetEmail(email)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(ForgotPassword.this, "Check email to reset your password!", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(ForgotPassword.this, "Fail to send reset password email!", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
     }
 }
+
 
 
 
